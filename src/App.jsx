4768494d4501a626fd23b4b5f95845637f6473db
dddddddimon <djdimavision@gmail.com>
2023-03-27 17:28:40 +0200

    19. Уроки React JS (route, browser-router)

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import Navigation from "./components/Navigation/Navigation";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navigation />
-      {/* <Profile /> */}
-      <Dialogs />
-    </div>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navigation />
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/dialogs" element={<Dialogs />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,19 +4,29 @@ function Navigation() {
   return (
     <nav className={s.nav}>
       <div className={`${s.item} ${s.active}`}>
-        <a className={s.linkProfile}>Profile</a>
+        <a href="/profile" className={s.linkProfile}>
+          Profile
+        </a>
       </div>
       <div className={`${s.item} ${s.active}`}>
-        <a className={s.linkProfile}>Messages</a>
+        <a href="/dialogs" className={s.linkProfile}>
+          Messages
+        </a>
       </div>
       <div className={`${s.item} ${s.active}`}>
-        <a className={s.linkProfile}>News</a>
+        <a href="/news" className={s.linkProfile}>
+          News
+        </a>
       </div>
       <div className={`${s.item} ${s.active}`}>
-        <a className={s.linkProfile}>Music</a>
+        <a href="/music" className={s.linkProfile}>
+          Music
+        </a>
       </div>
       <div className={`${s.item} ${s.active}`}>
-        <a className={s.linkProfile}>Settings</a>
+        <a href="/settings" className={s.linkProfile}>
+          Settings
+        </a>
       </div>
     </nav>
   );
