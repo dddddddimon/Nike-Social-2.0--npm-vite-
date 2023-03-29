a52c007c738f698628ea850f35025e6e5ce542af
dddddddimon <djdimavision@gmail.com>
2023-03-29 16:21:37 +0200

    21. Уроки React JS (Верстаем страницу Диалогов)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,7 +1,25 @@
 import s from "./Dialogs.module.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function Dialogs() {
-  return <div className={s.dialogstab}>Dialogs</div>;
+  return (
+    <div className={s.dialogstab}>
+      <div className={s.dialogsnames}>
+        <div className={`${s.dialog} ${s.active}`}>Sam</div>
+        <div className={s.dialog}>John</div>
+        <div className={s.dialog}>Petro</div>
+        <div className={s.dialog}>Ivan</div>
+        <div className={s.dialog}>Nika</div>
+        <div className={s.dialog}>Valera</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.massege}>Hey there!</div>
+        <div className={s.massege}>Yo man! What's up?</div>
+        <div className={s.massege}>Call me!</div>
+        <div className={s.massege}>Hello!</div>
+      </div>
+    </div>
+  );
 }
 
 export default Dialogs;
