82ac12fc2f63e7d93ab74066151ba2e7632f260e
dddddddimon <djdimavision@gmail.com>
2023-04-10 11:34:53 +0200

    30. Уроки React JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
@@ -7,30 +7,28 @@ import Dialogs from "./components/Dialogs/Dialogs";
 
 function App(props) {
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <Header />
-        <Navigation />
-        <Routes>
-          <Route
-            path="/profile"
-            element={<Profile posts={props.state.profilePage.posts} />}
-          />
-          <Route
-            path="/dialogs/*"
-            element={
-              <Dialogs
-                dialogs={props.state.messagesPage.dialogs}
-                messages={props.state.messagesPage.messages}
-              />
-            }
-          />
-          {/* <Route path="/news" element={<News />} />
+    <div className="app-wrapper">
+      <Header />
+      <Navigation />
+      <Routes>
+        <Route
+          path="/profile"
+          element={<Profile posts={props.state.profilePage.posts} />}
+        />
+        <Route
+          path="/dialogs/*"
+          element={
+            <Dialogs
+              dialogs={props.state.messagesPage.dialogs}
+              messages={props.state.messagesPage.messages}
+            />
+          }
+        />
+        {/* <Route path="/news" element={<News />} />
           <Route path="/music" element={<Music />} />
           <Route path="/settings" element={<Settings />} /> */}
-        </Routes>
-      </div>
-    </BrowserRouter>
+      </Routes>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import state from "./data/state";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App state={state} />
+    <BrowserRouter>
+      <App state={state} />
+    </BrowserRouter>
   </React.StrictMode>
 );
